9763e1129d21bdf45af3bdcf5961596be37268b7
杨正浩 <766727363@qq.com>
2015-07-02 19:56:04 +0800

    creattable

--- aWorkbench/frmCreateTable.cs
+++ aWorkbench/frmCreateTable.cs
@@ -11,9 +11,64 @@ namespace aWorkbench
 {
 	public partial class frmCreateTable : Form
 	{
+        frmCreateTable(string tablename) {
+            table_name.Text = tablename;//表名
+            //attribute
+            string[] att=new string[]{};
+            
+            this.comboBox2.Items.Add(att);
+            //type
+            string[] typ = new string[] { };
+
+            this.comboBox1.Items.Add(att);
+        }
 		public frmCreateTable()
 		{
 			InitializeComponent();
 		}
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void attribute_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void table_name_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void empty_Click(object sender, EventArgs e)
+        {
+            value.Text = null;
+            checkBox1.Checked = false;
+        }
+
+        private void submit_Click(object sender, EventArgs e)
+        {
+            bool n = checkBox1.Checked;
+            string val = textBox2.Text;
+            string attr = comboBox2.Text;
+            string typ = comboBox1.Text;
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
 	}
 }
