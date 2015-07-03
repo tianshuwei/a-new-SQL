5ca0771c4d374e47199482390e8c38cf8ac600c5
CR72727 <2773508392@qq.com>
2015-07-03 11:25:24 +0800

    frmConfirmScript&resultSet_todo

--- aWorkbench/frmConfirmScript.cs
+++ aWorkbench/frmConfirmScript.cs
@@ -12,10 +12,31 @@ namespace aWorkbench
 	public partial class frmConfirmScript : Form
 	{
 		public frmConfirmScript(string script) {
+            textBox1.Text = script;
 		}
 		public frmConfirmScript()
 		{
 			InitializeComponent();
 		}
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //cancel_btn
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //confirm_btn
+            string ipString = aWorkbench.cfg.ip;
+            int port = aWorkbench.cfg.port;
+            aWorkbench.aSQLConnector sendtxt = new aWorkbench.aSQLConnector(ipString,port);
+            sendtxt.send(textBox1.Text, true);
+        }
 	}
 }
