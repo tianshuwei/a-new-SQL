de27bd4179a8a72ba7850f8f7635eb79db654e5f
Alex Yang <aleozlx@126.com>
2015-07-06 09:46:50 +0000

    + shell

---
+++ aSQLServer/asql/shell.py
@@ -0,0 +1,25 @@
+import os
+from socket import *
+package=lambda *ts:''.join(['\x1B^','\x1B,'.join(ts),'\x1B$'])
+
+def send(lines):
+    sock=socket(AF_INET, SOCK_STREAM)
+    sock.connect(('localhost',1234))
+    sock.send(package('','\x20'.join(lines),''))
+    while 1:
+    	data=sock.recv(2048)
+    	if data: print data
+    	else: break
+    sock.close()
+
+try:
+    while 1:
+        lines=[]
+        try:
+        	while 1:
+        	    lines.append(raw_input())
+        	    if lines[-1].endswith(';'):
+        	        send(lines)
+        	        break
+        except EOFError: pass
+except KeyboardInterrupt: pass
