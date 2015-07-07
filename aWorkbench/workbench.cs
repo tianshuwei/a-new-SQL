33bce22cdcb4cb3e181f8a2e3fec43b261aea42d
张天毅 <tanki_jong@hotmail.com>
2015-07-07 10:06:06 +0800

    _(:з」∠)_。

--- aWorkbench/workbench.cs
+++ aWorkbench/workbench.cs
@@ -237,11 +237,5 @@ namespace aWorkbench
             this.lstConsoleMsg.Items.Add(lvi);
             lvi.SubItems.AddRange(new string[] { msg});
 		}
-
-        private void listViewResult_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
 	}
 }
